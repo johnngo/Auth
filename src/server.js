4b6c2e7a381ad630b2087aa5acc9fc91eebf1d3a
John <johnngo@Johns-MacBook-Pro.local>
2018-05-09 17:54:49 -0700

    completed authentication

--- src/server.js
+++ src/server.js
@@ -1,17 +1,27 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const session = require('express-session');
+const bcrypt = require('bcrypt');
 
+
+// Our requires
+const mongoose = require('mongoose');
 const User = require('./user');
+
 
 const STATUS_USER_ERROR = 422;
 const BCRYPT_COST = 11;
 
 const server = express();
+
 // to enable parsing of json bodies for post requests
-server.use(bodyParser.json());
+server.use(bodyParser.json());//server.use(express.json()) the way modern humans do this
+
+
 server.use(session({
-  secret: 'e5SPiqsEtjexkTj3Xqovsjzq8ovjfgVDFMfUzSmJO21dtXs4re'
+  secret: 'e5SPiqsEtjexkTj3Xqovsjzq8ovjfgVDFMfUzSmJO21dtXs4re',
+  saveUninitialized: false,
+  resave: false,                
 }));
 
 /* Sends the given err, a string or an object, to the client. Sets the status
@@ -25,17 +35,90 @@ const sendUserError = (err, res) => {
   }
 };
 
-// TODO: implement routes
-server.post('/users', (req, res) => {
-  const user = new User(req.body);
+const testUsername = function(req, res, next) {
+  const { username } = req.body;
+  
+  if (!username || username.trim() === '') {
+    return sendUserError("Username must exist and have a value!", res);
+  }
+  next();
+};
 
-  user.save().then(savedUser => res.status(200).json(savedUser)).catch(
-    err=> res.status(422).json(err));
-})
+const testPassword = function(req, res, next) {
+  const { password } = req.body;
+  
+  if (!password || password.trim() === '') {
+    return sendUserError("Password must exist and have a value!", res);
+  }
+  next();
+};
+
+const checkRestricted = function(req, res, next) {
+    
+    if(req.path === '/restricted') {
+      loginCheck(req ,res, next);
+      next();
+    }
+    next();
+  }
+
+server.use(checkRestricted);
+
+
+// TODO: implement routes
+
+  server.get('/', (req, res) => {
+    res.status(200).json({ api: 'running' });
+  });
+
+  server.post('/users', testPassword, (req, res) => {
+    const { username, password } = req.body;
+    const passwordHash = password;
+   
+    const user = new User({ username, passwordHash });
+
+    user
+    .save()
+    .then(savedUser => res.status(200).json(savedUser))
+    .catch(err => res.status(422).json(err));
+  });
+
+  server.post('/log-in', testUsername, testPassword, (req, res) => {
+    const { username, password } = req.body;
+    
+    User.findOne({ username })
+        .then(user => user.isPasswordValid(password))
+        .then(result => {
+          if (!result) {
+            return sendUserError("Password is invalid.", res);
+          }
+             req.session.name = username;
+             return res.status(200).json({ success: true });
+           })
+           .catch(err => sendUserError("Incorrect username.", res));
+  });
+
 // TODO: add local middleware to this route to ensure the user is logged in
-server.get('/me', (req, res) => {
+const loginCheck = function(req, res, next) {
+  const username = req.session.name;
+  
+  if (username) {
+    User.findOne({ username })
+        .then(user => {
+          req.user = user;
+          next();
+        })
+  } else {
+  return sendUserError("Access denied.", res);
+  }
+}
+
+server.get('/me', loginCheck, (req, res) => {
   // Do NOT modify this route handler in any way.
   res.json(req.user);
 });
+
+
+
 
 module.exports = { server };
--- src/user.js
+++ src/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+
 // Clear out mongoose's model cache to allow --watch to work for tests:
 // https://github.com/Automattic/mongoose/issues/1251
 mongoose.models = {};
@@ -10,17 +11,18 @@ mongoose.connect('mongodb://localhost/users', { useMongoClient: true });
 
 const UserSchema = new mongoose.Schema({
   // TODO: fill in this schema
-  username : {
-    type: String,    
-    unique: true,
-    required: true,
-    index: true,
-    lowercase: true,
-  },
-  passwordHash: {
-    type: String,
-    required: true
-  }
+
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      index: true,
+      lowercase: true,
+    },
+    passwordHash: {
+      type: String,
+      required: true,
+    }
 
 });
 
